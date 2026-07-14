63ee85c688eba22c51e23ab3984dc70e75f5e830
Rahat Karim <rahatkarim5452@gmail.com>
2026-07-14 22:15:15 +0600

    Practice

---
+++ practice.js
@@ -0,0 +1,22 @@
+console.log("Hello JavsScript!");
+console.log("i am excited to become a ......");
+
+let num1 = 25;
+let num2 = 10;
+console.log('Addition: '+(num1+num2));
+console.log('Subtraction: '+(num1-num2));
+console.log('Multiplication: '+num1*num2);
+console.log('Division: '+num1/num2);
+console.log('Modulus: '+num1%num2);
+
+let a = 100;
+let b = 'Programming';
+let c = true;
+let d = false;
+let e = 3.1416;
+
+console.log('a: '+typeof(a));
+console.log('b: '+typeof(b));
+console.log('c: '+typeof(c));
+console.log('d: '+typeof(d));
+console.log('e: '+typeof(e));
