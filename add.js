5d5326634e96e6d0886a739b87b1401c316db5d9
Rahat Karim <rahatkarim5452@gmail.com>
2026-07-14 21:44:46 +0600

    Add js

---
+++ add.js
@@ -0,0 +1,14 @@
+const orangePrice = 20;
+const mangoPriceString = '20';
+const mangoPrice = parseInt(mangoPriceString);
+const totalPrice = orangePrice + mangoPrice;
+console.log(orangePrice);
+console.log(mangoPrice);
+console.log(totalPrice);
+
+
+// Special case
+const firstNumber = 0.1;
+const secontNumber = 0.3;
+const total = firstNumber + secontNumber;
+console.log(total)
