2504568b4d595c27da4426b44e64a6ee15ffdf57
Rahat Karim <rahatkarim5452@gmail.com>
2026-07-14 21:10:49 +0600

    Variable related

---
+++ variable.js
@@ -0,0 +1,10 @@
+// VARIABLE old style 
+var weight = 90;
+var temp = 40;
+console.log(weight);
+
+// let,const 
+let price = 100;
+price= 50;
+price = 200;//Changable 
+const name = "Rahat";//Constant 
